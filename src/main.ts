c8ea0e1b47228a68d30167a30292d5ee90892b11
Ruben Spycher <rspycher@vtxmail.ch>
2020-01-08 09:18:49 +0100

    fix

--- src/main.ts
+++ src/main.ts
@@ -79,9 +79,10 @@ async function run() {
 
     console.log("Tools...")
     await exec.exec(`${pythonName} -m aqt tool`, [`-h`]);
+    await exec.exec(`${pythonName} -m aqt`, [`-h`]);
     if (tools_ifw) {
       let args = ["-O", `${dir}`,`${host}`, "tools_ifw"]
-      await exec.exec(`${pythonName} -m aqt tool`, args.concat(tools_ifw));
+      //await exec.exec(`${pythonName} -m aqt tool`, args.concat(tools_ifw));
     }
 
     //set environment variables
